acd51d0b36fe8dd80b0497761f3785b6d697567b
Dayna <daynajblackwell@gmail.com>
2018-11-01 23:36:13 -0700

    Added normalize.css.  Created modular structure for sass styling.  Added css theming file '_settings.scss'.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UncertaintyApp from './components/UncertaintyApp';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
   
 ReactDOM.render(<UncertaintyApp />, document.getElementById('app-root'));
