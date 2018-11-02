9053a49ab25cc8f87ba39de8ac7a3b818e3f8809
Dayna <daynajblackwell@gmail.com>
2018-11-01 21:22:52 -0700

    Added support for sass.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UncertaintyApp from './components/UncertaintyApp';
+import './styles/styles.scss';
   
 ReactDOM.render(<UncertaintyApp />, document.getElementById('app-root'));
